61bac036dc6c19722f5ee2d41a13d6248ae30ed6
MatthewMeacham305 <mmmeacha@ucsc.edu>
2023-07-08 10:56:19 -0700

    Added a lot of stuff
    Settings to change controls, outline shader, and new bullet

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] int PlaySceneID;
+    public Toggle ControlsToggle;
 
+    private void Start()
+    {
+        if (ControlsToggle != null)
+        {
+            Debug.Log(PlayerPrefs.GetInt("Controls"));
+            ControlsToggle.isOn = PlayerPrefs.GetInt("Controls", 0) == 1;
+        }
+    }
     public void Play(){
         StartCoroutine(DelayedPlay());
     }
@@ -24,4 +34,13 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+    public void SetControls()
+    {
+        if (ControlsToggle.isOn){
+            PlayerPrefs.SetInt("Controls", 1);
+        } else {
+            PlayerPrefs.SetInt("Controls", 0);
+        }
+        Debug.Log(PlayerPrefs.GetInt("Controls"));
+    }
 }
