17b0b107c99e36218bf807b5b6224cecfb13c497
AnthonyMolle <67618831+AnthonyMolle@users.noreply.github.com>
2023-07-09 03:06:07 -0700

    nice.....

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -122,6 +122,12 @@ public class BulletController : MonoBehaviour
             inBarrel = false;
             cameraController.ExitGun();
         }
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            FindObjectOfType<PauseMenu>().enemiesHit += 1;
+            Destroy(other.gameObject);
+        }
     }
 
     void Explode(){ 
